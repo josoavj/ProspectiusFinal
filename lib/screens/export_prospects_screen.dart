e397f85dad9db9ff2f545d27c69b9267cc68169a
Prospectius Dev <josoavonjiniaina13@gmail.com>
2025-11-29 20:12:46 +0300

    fix: Remove unused import from export_prospects_screen

--- lib/screens/export_prospects_screen.dart
+++ lib/screens/export_prospects_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../models/prospect.dart';
-import '../providers/auth_provider.dart';
 import '../providers/prospect_provider.dart';
 import '../services/excel_service.dart';
 
